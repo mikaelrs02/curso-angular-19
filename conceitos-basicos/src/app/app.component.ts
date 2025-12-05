7c9007ea86d98a8d25db9278f8c8bc32c8da000c
Mikael Ribeiro <mikaelribeirors@gmail.com>
2025-12-04 23:06:41 -0300

    Conceitos básicos concluídos

---
+++ conceitos-basicos/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculadoraComponent } from './calculadora/calculadora.component'
+import { ListaComprasComponent } from './lista-compras/lista-compras.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [ ListaComprasComponent ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'conceitos-basicos';
+}
