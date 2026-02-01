d7ef5b12d5e45233f538a6d2d80fa53bdb112819
Mikael Ribeiro <mikaelribeirors@gmail.com>
2026-02-01 17:16:57 -0300

    Configurando a alimentação de api

--- passeio-app/src/app/categorias/categoria/categoria.component.ts
+++ passeio-app/src/app/categorias/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { CategoriaService } from '../categoria.service';
 @Component({
   selector: 'app-categoria',
   standalone: false,
@@ -9,16 +10,30 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class CategoriaComponent {
   camposForm: FormGroup;
 
-  constructor() {
+  constructor(private service: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       descricao: new FormControl('', Validators.required)
     })
   }
   salvar(){
-    console.log(this.camposForm.value);
-    console.log(this.camposForm.valid);
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      this.service.salvar(this.camposForm.value).subscribe({
+        next: (categoria) => {
+          alert('Categoria salva com sucesso');
+          this.camposForm.reset();
+        },
+        error: (erro) => {
+          alert('Erro ao salvar categoria: ' + erro.message);
+        }
+      });   
+    } else {
+      this.camposForm.markAllAsTouched();
+    }
   }
+
   isCampoInvalido(campo: string): boolean {
     const campoControl = this.camposForm.get(campo);
     return campoControl ? campoControl.invalid && campoControl.touched && campoControl.errors?.['required'] : false;
