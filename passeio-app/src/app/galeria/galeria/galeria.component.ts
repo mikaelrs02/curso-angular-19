aaf2127737b49a2eae66d50608f9f19bb2ae86b2
Mikael Ribeiro <mikaelribeirors@gmail.com>
2026-02-04 00:01:33 -0300

    Criando a página de galeria com os cards dos lugares

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../../lugares/lugar.service';
 
 @Component({
   selector: 'app-galeria',
   standalone: false,
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+
+  lugares: Lugar[] = [];
+  categoriasFiltros: Categoria[] = [];
+
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe(categorias => this.categoriasFiltros = categorias)
+    this.lugarService.obterTodos().subscribe(lugaresResposta => this.lugares = lugaresResposta)
+  }
 
 }
