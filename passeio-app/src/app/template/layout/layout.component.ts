0789123ffbb7a4e6a91877e88472062e685bbbb7
Mikael Ribeiro <mikaelribeirors@gmail.com>
2026-02-06 23:39:29 -0300

    Capturando eventos de rotas para definir o titulo e subtitulo de cada página

--- passeio-app/src/app/template/layout/layout.component.ts
+++ passeio-app/src/app/template/layout/layout.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: false,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  props: LayoutProps = {
+    titulo: '',
+    subTitulo: ''
+  } 
+
+  constructor(private router: Router,private activatedRoute: ActivatedRoute) { 
+
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.props = this.obterPropriedadeLayout();
+    });
+  }
+
+  obterPropriedadeLayout(): LayoutProps {
+    let rotaFilha = this.activatedRoute.firstChild;
+    while(rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 
 }
+
